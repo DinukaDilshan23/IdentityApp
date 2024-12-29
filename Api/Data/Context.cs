dbc6af9e38e9403f25d5884338624d1205230f76
DinukaD97 <dinuka.221b@gmail.com>
2024-12-29 10:56:50 +0530

    Api project setup, AspNetCore Identity added

---
+++ Api/Data/Context.cs
@@ -0,0 +1,11 @@
+﻿using Api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class Context(DbContextOptions<Context> options) : IdentityDbContext<User>(options)
+    {
+    }
+}
---
+++ Api/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Models
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public String LastName { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+    }
+}
