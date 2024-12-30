2a98c5212ae27ac81bb4cdce2018903c91c6e059
DinukaD97 <dinuka.221b@gmail.com>
2024-12-30 15:04:55 +0530

    03. User Persistence in Angular

--- ClientApp/src/app/account/login/login.component.ts
+++ ClientApp/src/app/account/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AccountService } from '../account.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { take } from 'rxjs';
+import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-login',
@@ -13,15 +15,33 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup = new FormGroup({});
   submitted = false;
   errorMessages: string[] = [];
+  returnUrl: string | null = null;
 
   constructor(private accountService: AccountService,
     private formBuilder: FormBuilder,
-    private router: Router) { }
-
-    ngOnInit(): void {
-      console.log('RegisterComponent initialized');
-      this.initializeForm();
+    private router: Router,
+    private activatedRoute: ActivatedRoute) {
+      this.accountService.user$.pipe(take(1)).subscribe({
+      next: (user: User | null) => {
+        if (user) {
+          this.router.navigateByUrl('/');
+        } else {
+          this.activatedRoute.queryParamMap.subscribe({
+            next: (params: any) => {
+              if (params) {
+                this.returnUrl = params.get('returnUrl');
+              }
+            }
+          })
+        }
+      }
+      })
     }
+
+  ngOnInit(): void {
+    console.log('RegisterComponent initialized');
+    this.initializeForm();
+  }
 
   initializeForm() {
     this.loginForm = this.formBuilder.group({
@@ -37,7 +57,11 @@ export class LoginComponent implements OnInit {
     if (this.loginForm.valid) {
       this.accountService.login(this.loginForm.value).subscribe({
         next: (response: any) => {
-          
+          if (this.returnUrl) {
+            this.router.navigateByUrl(this.returnUrl);
+          } else {
+            this.router.navigateByUrl('/')
+          }
         },
         error: error => {
           if (error.error.errors) {
