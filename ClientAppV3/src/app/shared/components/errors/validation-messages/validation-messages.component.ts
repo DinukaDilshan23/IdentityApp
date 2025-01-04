4dccd0f46a4812c617d5e5f4f7dd43735ddeb0ce
DinukaD97 <dinuka.221b@gmail.com>
2025-01-04 23:48:39 +0530

    01. ClientAppV3 Earlier to Add Bootswatch

---
+++ ClientAppV3/src/app/shared/components/errors/validation-messages/validation-messages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validation-messages',
+  standalone: false,
+  
+  templateUrl: './validation-messages.component.html',
+  styleUrl: './validation-messages.component.css'
+})
+export class ValidationMessagesComponent {
+
+}
