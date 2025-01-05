db1efbcaabc75aaedee625d775aa96888dfce8b8
DinukaD97 <dinuka.221b@gmail.com>
2025-01-05 21:13:33 +0530

    06. Completed the Section 04 - User Persistance in ClientAppV3

--- ClientAppV3/src/app/account/register/register.component.ts
+++ ClientAppV3/src/app/account/register/register.component.ts
@@ -1,36 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../account.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { SharedService } from '../../shared/shared.service';
+import { SharedService } from '../../../app/shared/shared.service';
 import { Router } from '@angular/router';
-
+import { take } from 'rxjs';
+import { User } from '../../../app/shared/models/user';
 
 @Component({
   selector: 'app-register',
   standalone: false,
-
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup = new FormGroup({});
   submitted = false;
   errorMessages: string[] = [];
+
   constructor(private accountService: AccountService,
     private sharedService: SharedService,
     private formBuilder: FormBuilder,
-    private router: Router) { }
+    private router: Router) {
+    this.accountService.user$.pipe(take(1)).subscribe({
+      next: (user: User | null) => {
+        if (user) {
+          this.router.navigateByUrl('/');
+        }
+      }
+    })
+  }
+
   ngOnInit(): void {
     this.initializeForm();
   }
+
   initializeForm() {
     this.registerForm = this.formBuilder.group({
       firstName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(15)]],
       lastName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(15)]],
       email: ['', [Validators.required, Validators.pattern('^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$')]],
-      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(15)]]
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(15)]],
     })
   }
+
   register() {
     this.submitted = true;
     this.errorMessages = [];
@@ -50,5 +62,6 @@ export class RegisterComponent implements OnInit {
         }
       })
     }
+
   }
 }
--- ClientAppV3/src/app/play/play.component.ts
+++ ClientAppV3/src/app/play/play.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { PlayService } from './play.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-play',
-  standalone: false,
-  
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './play.component.html',
-  styleUrl: './play.component.css'
+  styleUrls: ['./play.component.css']
 })
-export class PlayComponent {
+export class PlayComponent implements OnInit {
+  message: string | undefined;
+
+  constructor(private playService: PlayService) { }
+
+  ngOnInit(): void {
+    this.playService.getPlayers().subscribe({
+      next: (respose: any) => this.message = respose.value.message,
+      error: error => console.log(error)
+    })
+  }
 
 }
